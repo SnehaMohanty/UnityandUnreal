cdec3fbca43d7731d9ddf89d23238a16f58932d2
Sneha Mohanty <sm21483@gmail.com>
2020-11-10 16:20:27 +0100

    add lights effect and cube colors

---
+++ lights/Assets/ChangeColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Material nMaterial;
+  
+    public void setColorToBlack()
+    {
+        nMaterial.SetColor("_Color", Color.black);
+    }
+
+    public void setColorToWhite()
+    {
+        nMaterial.SetColor("_Color", Color.white);
+    }
+
+    public void setColorToRed()
+    {
+        nMaterial.SetColor("_Color", Color.red);
+    }
+
+    public void setColorToGreen()
+    {
+        nMaterial.SetColor("_Color", Color.green);
+    }
+}
---
+++ lights/Assets/multicolors.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class multicolors : MonoBehaviour
+{
+
+    MeshRenderer cubeMeshrenderer;
+
+    [SerializeField] [Range(0f, 1f)] float lerpTime;
+    [SerializeField] Color[] myColors;
+
+    int colorIndex = 0;
+
+    float t = 0f;
+
+    int len;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeMeshrenderer = GetComponent<MeshRenderer>();
+        len = myColors.Length;
+        lerpTime = 5;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        cubeMeshrenderer.material.color = Color.Lerp(cubeMeshrenderer.material.color,myColors[colorIndex], lerpTime*Time.deltaTime);
+
+        t = Mathf.Lerp(t, 1f, lerpTime * Time.deltaTime);
+        if (t > 0.9f)
+        {
+            t = 0f;
+            colorIndex++;
+            colorIndex = (colorIndex >= myColors.Length) ? 0 : colorIndex;
+        }
+    
+    }
+}
