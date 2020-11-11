a91dbf48ee14d6078629a93a87a504f6d645b04f
Sneha Mohanty <sm21483@gmail.com>
2020-11-11 17:44:46 +0100

    Ray reflecting surface

---
+++ rayreflectingphysics/Assets/RayReflect.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class RayReflect : MonoBehaviour
+{
+    //this game object's Transform  
+    private Transform goTransform;
+    //the attached line renderer  
+    private LineRenderer lineRenderer;
+
+    //a ray  
+    private Ray ray;
+    //a RaycastHit variable, to gather informartion about the ray's collision  
+    private RaycastHit hit;
+
+    //reflection direction  
+    private Vector3 inDirection;
+
+    //the number of reflections  
+    public int nReflections = 2;
+
+    //the number of points at the line renderer  
+    private int nPoints;
+
+    void Awake()
+    {
+        //get the attached Transform component  
+        goTransform = this.GetComponent<Transform>();
+        //get the attached LineRenderer component  
+        lineRenderer = this.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        //clamp the number of reflections between 1 and int capacity  
+        nReflections = Mathf.Clamp(nReflections, 1, nReflections);
+        //cast a new ray forward, from the current attached game object position  
+        ray = new Ray(goTransform.position, goTransform.forward);
+
+        //set the number of points to be the same as the number of reflections  
+        nPoints = nReflections;
+        //make the lineRenderer have nPoints  
+        lineRenderer.SetVertexCount(nPoints);
+        //Set the first point of the line at the current attached game object position  
+        lineRenderer.SetPosition(0, goTransform.position);
+
+        for (int i = 0; i <= nReflections; i++)
+        {
+            //If the ray hasn't reflected yet  
+            if (i == 0)
+            {
+                //Check if the ray has hit something  
+                if (Physics.Raycast(ray.origin, ray.direction, out hit, 100))//cast the ray 100 units at the specified direction  
+                {
+                    //the reflection direction is the reflection of the current ray direction flipped at the hit normal  
+                    inDirection = Vector3.Reflect(ray.direction, hit.normal);
+                    //cast the reflected ray, using the hit point as the origin and the reflected direction as the direction  
+                    ray = new Ray(hit.point, inDirection);
+
+                    //if the number of reflections is set to 1  
+                    if (nReflections == 1)
+                    {
+                        //add a new vertex to the line renderer  
+                        lineRenderer.SetVertexCount(++nPoints);
+                    }
+
+                    //set the position of the next vertex at the line renderer to be the same as the hit point  
+                    lineRenderer.SetPosition(i + 1, hit.point);
+                }
+            }
+            else // the ray has reflected at least once  
+            {
+                //Check if the ray has hit something  
+                if (Physics.Raycast(ray.origin, ray.direction, out hit, 100))//cast the ray 100 units at the specified direction  
+                {
+                    //the refletion direction is the reflection of the ray's direction at the hit normal  
+                    inDirection = Vector3.Reflect(inDirection, hit.normal);
+                    //cast the reflected ray, using the hit point as the origin and the reflected direction as the direction  
+                    ray = new Ray(hit.point, inDirection);
+
+                    //add a new vertex to the line renderer  
+                    lineRenderer.SetVertexCount(++nPoints);
+                    //set the position of the next vertex at the line renderer to be the same as the hit point  
+                    lineRenderer.SetPosition(i + 1, hit.point);
+                }
+            }
+        }
+    }
+}
