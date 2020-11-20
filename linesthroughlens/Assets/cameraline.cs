91c20cd92a65379e7b01defd0d8d609a4ce98bb8
Sneha Mohanty <sm21483@gmail.com>
2020-11-20 22:43:30 +0100

    linespassingthroughlenses

---
+++ linesthroughlens/Assets/cameraline.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraline : MonoBehaviour
+{
+  
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit; if (Physics.Raycast(ray, out hit, 100))
+            Debug.DrawLine(ray.origin, hit.point);
+    }
+}
---
+++ linesthroughlens/Assets/multipleray.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class multipleray : MonoBehaviour
+{
+    // The target marker.
+    public Transform target;
+
+    // Angular speed in radians per sec.
+    public float speed = 0.5f;
+
+
+    void Update()
+    {
+
+        //float speed = 0.5f;
+
+        Ray ray = new Ray(transform.position + Vector3.up, transform.forward);
+        var fwd = transform.TransformDirection(Vector3.down);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 0.5f))
+        {
+
+
+            
+            //ray.direction = Quaternion.AngleAxis(12.0f, Vector3.forward) * Vector3.right;
+            transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * speed*0.6f);
+            //Debug.DrawRay(transform.position, hit.point, Color.red);
+
+        }
+        else
+        {
+            //Debug.DrawRay(transform.position, hit.point, Color.white);
+            transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * speed * 0.6f);
+            transform.position += transform.forward * speed* Time.deltaTime;
+            
+        }
+
+
+
+    }
+
+}
